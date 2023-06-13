e6705c4d1bebe0b5c11534c33711b0efa8f2f8f0
Chaeyeon Seo <ssallyseo@gmail.com>
2023-06-13 16:54:38 +0900

    [Feat] : 회원가입시 이메일 인증 화면 구현
    - 이메일 인증 링크 클릭 시 넘어가는 페이지 구현
    - 프로필 페이지 파일 생성

---
+++ static/js/confirm_email.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    alert("이메일 인증이 완료되었습니다. 홈페이지로 돌아가서 로그인해주세요.");
+    window.close();
+  };
+  
+ 
--- static/js/login_api.js
+++ static/js/login_api.js
@@ -1,10 +1,15 @@
+if (localStorage.getItem("kakao") || localStorage.getItem("google") || localStorage.getItem("naver") || localStorage.getItem("payload")) {
+    window.location.replace("index.html")
+    alert("로그인됨")
+}
+
 
 //로그인
 async function Login() {
     const account = document.getElementById("account").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch(`${backend_base_url}/user/login/`, { 
+    const response = await fetch(`${BACKEND_BASE_URL}/user/login/`, { 
             headers: {
                 'content-type': 'application/json'
             },
@@ -48,11 +53,11 @@ async function Login() {
 
 // 카카오로그인
 async function kakaoLogin(){
-    const response = await fetch(`${backend_base_url}/user/login/kakao/`, {
+    const response = await fetch(`${BACKEND_BASE_URL}/user/login/kakao/`, {
         method : "GET"
     })
     const kakao_id = await response.json()
-    const redirect_uri =`${frontend_base_url}/index.html`
+    const redirect_uri =`${FRONTEND_BASE_URL}/index.html`
     const scope = 'profile_nickname account_email age_range'
     window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}`
 
@@ -60,11 +65,11 @@ async function kakaoLogin(){
 
 // 네이버로그인
 async function naverLogin(){
-    const response = await fetch(`${backend_base_url}/user/login/naver/`, {
+    const response = await fetch(`${BACKEND_BASE_URL}/user/login/naver/`, {
         method : "GET"
     });
     const naver_id = await response.json();
-    const redirect_uri =`${frontend_base_url}/index.html`
+    const redirect_uri =`${FRONTEND_BASE_URL}/index.html`
     const state = new Date().getTime().toString(36); // 랜덤 state : 현재 시간을 36진수로 변환, CSRF 공격 방지
     window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naver_id}&redirect_uri=${redirect_uri}&state=${state}`;
     
@@ -74,25 +79,14 @@ async function naverLogin(){
 
 // 구글로그인
 async function googleLogin(){
-    const response = await fetch(`${backend_base_url}/user/login/google/`, {
+    const response = await fetch(`${BACKEND_BASE_URL}/user/login/google/`, {
         method : "GET"
     });
     const google_id = await response.json();
-    const redirect_uri =`${frontend_base_url}/index.html`
+    const redirect_uri =`${FRONTEND_BASE_URL}/index.html`
     const scope = `https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`
     const param = `scope=${scope}&include_granted_scopes=true&response_type=token&state=pass-through value&prompt=consent&client_id=${google_id}&redirect_uri=${redirect_uri}`
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${param}`
 }
 
 
-function go_signup(){
-    location.href = "signup.html"
-}
-
-function go_findAccount(){
-    location.href = "find_account.html"
-}
-
-function go_findPassword(){
-    location.href = "find_password.html"
-}
