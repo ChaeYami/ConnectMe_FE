b4011f02bca139e2e8ecbe3bc80acd8ee6dbabc0
Chaeyeon Seo <ssallyseo@gmail.com>
2023-06-15 11:25:51 +0900

    [Feat] : 홈에서 유저 추천 목록

--- static/js/recommend.js
+++ static/js/recommend.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 // 추천 유저목록
 async function recommend(filter) {
     $('#list-section').empty()
+    $('.condition').empty()
 
     $.ajax({
         url: `${BACKEND_BASE_URL}/user/recommend/${filter}/`,
@@ -18,28 +19,43 @@ async function recommend(filter) {
         success: function (response) {
             const rows = response;
             if (rows[0] == undefined) {
-                console.log('없음')
-                if (filter == 'age'){
+                if (filter == 'age') {
                     filter = '나이가'
-                }else if (filter == 'mbti'){
+                } else if (filter == 'mbti') {
                     filter = 'MBTI가'
-                }else if (filter == 'region'){
+                } else if (filter == 'region') {
                     filter = '지역이'
                 }
                 let temp_html = `해당 조건에 맞는 유저가 존재하지 않거나 프로필에 설정된 ${filter} 없습니다.`
                 $('#list-section').append(temp_html)
 
             } else {
+                let filter_ =''
+                if (filter == 'age') {
+                    filter_ = '나이'
+                } else if (filter == 'mbti') {
+                    filter_ = 'MBTI'
+                } else if (filter == 'region') {
+                    filter_ = '지역'
+                }else if(filter == 'all'){
+                    filter_ = '전체'
+                }
+                let condition = `${filter_} : ${rows[0][filter]}`
+                $('.condition').append(condition)
+
                 console.log(rows[0])
                 for (let i = 0; i < rows.length; i++) {
+                    let user_pk = rows[i]['id']
                     let user_nickname = rows[i]['nickname']
                     let user_region = rows[i]['prefer_region']
                     let user_age = rows[i]['age']
                     let user_mbti = rows[i]['mbti']
                     let user_introduce = rows[i]['introduce']
-                    let user_profile_img=rows[i]['profile_img']
-
-                    let temp_html = `<div class="card">
+                    let user_profile_img = rows[i]['profile_img']
+                    if (user_profile_img == null) {
+                        user_profile_img = '/media/user.png'
+                    }
+                    let temp_html = `<a onclick="go_profile(${user_pk})"><div class="card">
                     <div class="image_box">
                         <img class="image" src="${BACKEND_BASE_URL}${user_profile_img}" style = "width : 200px;"alt="">
                     </div>
@@ -56,7 +72,7 @@ async function recommend(filter) {
                             ${user_introduce}
                         </div>
                     </div>
-                </div>`
+                </div></a>`
 
                     $('#list-section').append(temp_html)
                 }
