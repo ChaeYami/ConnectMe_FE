6f2de38bb16cd20a7627bb68299cb407ddd1df1a
Chaeyeon Seo <ssallyseo@gmail.com>
2023-06-16 18:53:52 +0900

    [Feat] : 아이디 찾기 구현

---
+++ static/js/find_account_api.js
@@ -0,0 +1,63 @@
+if (localStorage.getItem("payload")){
+    window.location.replace("index.html")
+    alert("접근이 불가합니다.")
+} else {}
+
+
+//휴대폰 인증번호 발송
+async function phone_send() {
+    const phone_send_data = {
+        phone: document.getElementById("phone").value,
+    }
+
+    const response = await fetch(`${BACKEND_BASE_URL}/user/phone/send/`,{
+        headers:{
+            'Content-type':'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(phone_send_data)
+    })
+
+    const result = await response.json()
+    
+    if (response.status === 200) {
+        alert("인증번호가 발송되었습니다. 확인부탁드립니다.")
+
+    } else if (response.status === 400) {
+        document.getElementById('alert-danger_1').style.display ="block"
+        const alert_danger_1 = document.getElementById('alert-danger_1')
+        alert_danger_1.innerText = `${result['message']}`
+
+        
+    }
+}
+
+
+//인증번호 확인
+async function auth_number_confirm() {
+    const auth_number_confirm_data = {
+        phone: document.getElementById("phone").value,
+        auth_number: document.getElementById("auth_number").value,
+    }
+
+    const response = await fetch(`${BACKEND_BASE_URL}/user/phone/verify/account/`,{
+        headers:{
+            'Content-type':'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(auth_number_confirm_data)
+    })
+
+    const result = await response.json()
+    
+    if (response.status === 200) {
+        document.getElementById('info_message').style.display ="block"
+        const alert_danger_2 = document.getElementById('info_message')
+        alert_danger_2.innerText = `${result['message']}`
+        
+    } else if (response.status === 400) {
+        document.getElementById('alert-danger_2').style.display ="block"
+        const alert_danger_2 = document.getElementById('alert-danger_2')
+        alert_danger_2.innerText = `${result['message']}`
+    }
+}
--- static/js/friends_list.js
+++ static/js/friends_list.js
@@ -3,7 +3,7 @@ window.onload = () => {
     friendsList()
 }
 
-
+// 친구목록 user/friend/list/
 async function friendsList() {
     $('.list-section').empty()
 
@@ -31,17 +31,42 @@ async function friendsList() {
                 let request_id = rows[i]['id']
 
                 let temp_html = `
-                    <div class="info-box">
-                        <a onclick="go_profile(${user_id})">
-                            <div class="nickname">${user_nickname}</div>
-                            <div class="account">${user_account}</div>
-                        </a>
-                    </div>
+                <div class="info-box">
+                    <a onclick="go_profile(${user_id})">
+                        <div class="nickname">${user_nickname}</div>
+                        <div class="account">${user_account}</div>
+                    </a>
+                    <button onclick="deleteFriendButton(${request_id})">친구끊기</button>
+                </div>
                 `
 
                 $('.list-section').append(temp_html)
 
             }
+        }
+    })
+}
+
+// 친구삭제 user/friend/id/delete/
+function deleteFriendButton(request_id) {
+    if (confirm("친구를 끊으시겠습니까?")) {
+        deleteFriend(request_id);
+    }
+}
+async function deleteFriend(request_id) {
+    $.ajax({
+        url: `${BACKEND_BASE_URL}/user/friend/${request_id}/delete/`,
+        type: "DELETE",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + logined_token
+        },
+        success: function(response){
+            alert(response['message'])
+            location.reload()
+        },
+        error : function(response){
+            alert(response['responseJSON']['message'])
         }
     })
 
