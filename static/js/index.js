3bbcc43d191fee66ac44873fcadea4d94826f1ef
sangmin <leejeayu05@gmail.com>
2023-06-22 09:33:31 +0900

    [Design] : 상세페이지 css
    -place_view 수정 : 위, 아래 스크롤 aside태그로 이동
    -base.css 수정: h2 margin 값 삭제
    -place.css 수정 : 이미지 추가하기 수정, 추가하기 버튼 추가, 이미지 미리보기 수정, 전체보기 수정
    -place.css 추가 : 이미지 슬라이더 추가, 상세 페이지 추가
    -index.js 추가 : 핫플레이스 추천 추가
    -place_api.js 수정 : 상세페이지 수정

--- static/js/index.js
+++ static/js/index.js
@@ -21,7 +21,8 @@ if (localStorage.getItem("payload")) {
 const logined_token = localStorage.getItem("access");
 
 $(document).ready(function () {
-    recommend('all')
+    recommend('all');
+    recommendHotPlace();
 });
 
 
@@ -110,7 +111,7 @@ async function recommend(filter) {
                 let user_nickname = rows[i]['nickname']
                 let user_region = rows[i]['prefer_region']
                 let user_profile_img = rows[i]['profile_img']
-                if (user_profile_img==null){
+                if (user_profile_img == null) {
                     user_profile_img = '/media/user.png'
                 }
 
@@ -138,4 +139,112 @@ async function recommend(filter) {
         }
     })
 
+}
+
+// 추천 핫플레이스
+async function recommendHotPlace() {
+    let container = document.querySelector('#place-section')
+
+    const response = await fetch(`${BACKEND_BASE_URL}/place/`, {
+        headers: {
+            Authorization: "Bearer " + logined_token,
+        },
+        method: "GET",
+    });
+
+    const response_json = await response.json();
+
+    response_json.forEach((e, i) => {
+        let place_id = e.id
+        let name = e.title
+        let category = e.category
+        let content = e.content
+        let address = e.address
+        let image = e.image
+        let score = e.score
+        let bookmark = e.bookmark
+
+        container.innerHTML += `
+        <div id="place${place_id}" class="place-container"></div>`
+
+        let place = document.querySelector(`#place${place_id}`)
+
+        // 이미지 시작
+        if (image) {
+            place.innerHTML += `
+            <div>
+                <a href="place_view.html?id=${place_id}">
+                    <img class="place-container-img" src="${image['url']}" onclick="placePreUpdateView()">
+                </a>
+            </div>
+            `
+        } else {
+            place.innerHTML += `
+            <div style="width:230px; height:230px;">
+            </div>`
+        }
+        // 이미지 끝
+        // 북마크 시작
+        let place_book = ``
+
+        if (bookmark.includes(logined_user_id)) {
+
+            place_book = `
+            <a>
+                <img id="book${place_id}" src="static/image/bookmark (1).png" style="margin-top:10px; width: 40px;" alt="북마크" onclick="placeBook(${place_id})">
+            </a>`
+        } else {
+            place_book = `
+            <a>
+                <img id="book${place_id}" src="static/image/bookmark.png" style="margin-top:10px; width: 40px;" alt="북마크" onclick="placeBook(${place_id})">
+            </a>`
+        }
+        // 북마크 끝
+        // edit 버튼 시작
+        let place_edit = ``
+
+        if (JSON.parse(payload)['is_staff']) {
+            place_edit = `
+            <a>
+                <img src="static/image/edit.png" style="margin-top:10px; width:20px;"
+                    onclick="placePreUpdateView(${place_id})">
+            </a>
+            `
+        }
+        // edit 버튼 끝
+        // container html 시작
+        place.innerHTML += `
+        <div class="place-container-text">
+            <div class="place-container-main">
+                <div class="place-container-title">
+                    <div class="place-container-title0">
+                        <h2>${i + 1}.</h2>
+                    </div>
+                    <div class="place-container-title1">
+                        <h2><a class="place-container-title-a" href="place_view.html?id=${place_id}">${name}</a></h2>
+                    </div>
+                    <div class="place-container-title2">
+                        <div class="place-container-score">
+                            <h2>${score}</h2>
+                        </div>
+                    </div>
+                    <div id="place_edit">
+                    ${place_edit}
+                    </div>
+                </div>
+                <div class="place-container-book" id="place-container-book">
+                ${place_book}
+                </div>
+            </div>
+            <div class="place-container-address">${address}</div>
+            <div class="place-container-content">${content}</div>
+            <div id="map"></div>
+            
+        </div>
+        <div class="place-container-hr">
+            <hr>
+        </div>
+        `
+        // container html 끝
+    })
 }
--- static/js/resource.js
+++ static/js/resource.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
     });
 });
 
-//이미지 미리보기
+// ##############이미지 미리보기 시작#################
 function setThumbnail(event) {
     var container = document.querySelector("#image_container");
     container.innerHTML = '';
@@ -47,3 +47,47 @@ function setThumbnail(event) {
         reader.readAsDataURL(image);
     }
 }
+// ##############이미지 미리보기 끝#################
+
+// ##############이미지 슬라이더 시작#################
+let pages = 0;//현재 인덱스 번호
+let positionValue = 0;//images 위치값
+const IMAGE_WIDTH = 250;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
+//DOM
+const backBtn = document.querySelector(".back")
+const nextBtn = document.querySelector(".next")
+const images = document.querySelector(".images")
+
+function next() {
+    if (pages < 3) {
+        backBtn.removeAttribute('disabled')//뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
+        positionValue -= IMAGE_WIDTH;//IMAGE_WIDTH의 증감을 positionValue에 저장한다.
+        images.style.transform = `translateX(${positionValue}px)`;
+        //x축으로 positionValue만큼의 px을 이동한다.
+        pages += 1; //다음 페이지로 이동해서 pages를 1증가 시킨다.
+    }
+    if (pages === 3) { //
+        nextBtn.setAttribute('disabled', 'true')//마지막 장일 때 next버튼이 disabled된다.
+    }
+}
+
+function back() {
+    if (pages > 0) {
+        nextBtn.removeAttribute('disabled')
+        positionValue += IMAGE_WIDTH;
+        images.style.transform = `translateX(${positionValue}px)`;
+        pages -= 1; //이전 페이지로 이동해서 pages를 1감소 시킨다.
+    }
+    if (pages === 0) {
+        backBtn.setAttribute('disabled', 'true')//마지막 장일 때 back버튼이 disabled된다.
+    }
+}
+
+function init() {  //초기 화면 상태
+    backBtn.setAttribute('disabled', 'true'); //속성이 disabled가 된다.
+    backBtn.addEventListener("click", back); //클릭시 다음으로 이동한다.
+    nextBtn.addEventListener("click", next);//클릭시 이전으로 이동한다.
+}
+init();
+
+// ##############이미지 슬라이더 끝#################
